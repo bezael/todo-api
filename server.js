ec4f1b377ff19d59e9e98606246c082bf520a9a1
Bezael Pérez <bezael_e@hotmail.com>
2017-11-11 14:58:14 +0100

    change console.log server

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 
+let todos =[{
+	description: 'Meet mom for lunch',
+	completed: false
+}];
+
+
 
 app.get('/', function(req, res){
 	res.send('Todo-api ROOT');
 });
 
 app.listen(port, function(){
-	console.log(`Server running on port: ${port}`);
+	console.log('Server running on port: '+ port);
 });
