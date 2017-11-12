f9460e8a53f160715bf332f32f57d4d607bcd0de
Bezael Pérez <bezael_e@hotmail.com>
2017-11-12 13:30:40 +0100

    reFactor GET /todos/? params use to sequelize

--- server.js
+++ server.js
@@ -14,26 +14,26 @@ app.use(express.static(__dirname + '/public'));
 
 //GET /todos?completed=true&q=house
 app.get('/todos', (req, res) => {
-	let queryParams = req.query;
-	let filteredTodos = todos;
-
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: true
-		});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: false
-		});
+	let query = req.query;
+	let where={};
+	
+	if(query.hasOwnProperty('completed') && query.completed === 'true'){
+		where.completed=true;
+	}else if(query.hasOwnProperty('completed') && query.completed==='false'){
+		where.completed=false;
 	}
 
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, (todo) => {
-			return todo.description.toLowerCase().indexOf(queryParams.q) > -1;
-		});
+	if(query.hasOwnProperty('q') && query.q.length > 0){
+		where.description = {
+			$like: '%' + query.q + '%'
+		};
 	}
+	db.todo.findAll({where: where}).then(function(todos){
+		res.json(todos);
+	}, function(e){
+		res.status(500).send();
+	});
 
-	res.json(filteredTodos);
 });
 
 //GET /todos/:id
